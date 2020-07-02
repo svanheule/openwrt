95ddb509c45f83f560220f4c01ef35c4b952ef9a
Sander Vanheule <sander@svanheule.net>
2020-07-02 22:33:56 +0200

    kernel: mtdsplit: support ELF loader splitting
    To parse the ELF kernel loader, a minimal ELF parser is used that can
    handle both ELF32 or ELF64 class loaders. The splitter assumes that the
    kernel is always located before the rootfs, whether it is embedded in
    the loader or not. If the kernel is located after the rootfs on the
    firmware partition, then the rootfs splitter will include it in the
    dynamically created rootfs_data partition and the kernel will be
    corrupted.
    
    The kernel image is preferably embedded inside the ELF loader, so the
    end of the loader equals the end of the kernel partition. This is due to
    the way mtd_find_rootfs_from searches for the the rootfs:
    - if the kernel image is embedded in the loader, the appended rootfs may
      follow the loader immediately, within the same erase block.
    - if the kernel image is not embedded in the loader, but placed at some
      offset behind the loader (OKLI-style loader), the rootfs must be
      aligned to an erase-block after the loader and kernel image.
    
    Signed-off-by: Sander Vanheule <sander@svanheule.net>

---
+++ target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_elf.c
@@ -0,0 +1,234 @@
+/*
+ *  MTD splitter for ELF loader firmware partitions
+ *
+ *  Copyright (C) 2020 Sander Vanheule <sander@svanheule.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ *
+ *  To parse the ELF kernel loader, a minimal ELF parser is used that can
+ *  handle both ELF32 or ELF64 class loaders. The splitter assumes that the
+ *  kernel is always located before the rootfs, whether it is embedded in the
+ *  loader or not. If the kernel is located after the rootfs on the firmware
+ *  partition, then the rootfs splitter will include it in the dynamically
+ *  created rootfs_data partition and the kernel will be corrupted.
+ *
+ *  The kernel image is preferably embedded inside the ELF loader, so the end
+ *  of the loader equals the end of the kernel partition. This is due to the
+ *  way mtd_find_rootfs_from searches for the the rootfs:
+ *  - if the kernel image is embedded in the loader, the appended rootfs may
+ *    follow the loader immediately, within the same erase block.
+ *  - if the kernel image is not embedded in the loader, but placed at some
+ *    offset behind the loader (OKLI-style loader), the rootfs must be
+ *    aligned to an erase-block after the loader and kernel image.
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
+#include <linux/of.h>
+#include <linux/byteorder/generic.h>
+
+#include "mtdsplit.h"
+
+#define ELF_NR_PARTS	2
+
+#define ELF_MAGIC		0x7f454c46 /* 0x7f E L F */
+#define ELF_CLASS_32	1
+#define ELF_CLASS_64	2
+
+struct elf_header_ident {
+	uint32_t magic;
+	uint8_t class;
+	uint8_t data;
+	uint8_t version;
+	uint8_t osabi;
+	uint8_t abiversion;
+	uint8_t pad[7];
+};
+
+struct elf_header_32 {
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version;
+	uint32_t entry;
+	uint32_t phoff;
+	uint32_t shoff;
+	uint32_t flags;
+	uint16_t ehsize;
+	uint16_t phentsize;
+	uint16_t phnum;
+	uint16_t shentsize;
+	uint16_t shnum;
+	uint16_t shstrndx;
+};
+
+struct elf_header_64 {
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version;
+	uint64_t entry;
+	uint64_t phoff;
+	uint64_t shoff;
+	uint32_t flags;
+	uint16_t ehsize;
+	uint16_t phentsize;
+	uint16_t phnum;
+	uint16_t shentsize;
+	uint16_t shnum;
+	uint16_t shstrndx;
+};
+
+
+
+static int mtdsplit_elf_read_mtd(struct mtd_info *mtd,
+				size_t offset, uint8_t *dst, size_t header_len)
+{
+	size_t retlen;
+	int ret;
+
+	ret = mtd_read(mtd, offset, header_len, &retlen, dst);
+	if (ret) {
+		pr_debug("read error in \"%s\"\n", mtd->name);
+		return ret;
+	}
+
+	if (retlen != header_len) {
+		pr_debug("short read in \"%s\"\n", mtd->name);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int mtdsplit_parse_elf(struct mtd_info *mtd,
+				const struct mtd_partition **pparts,
+				struct mtd_part_parser_data *data)
+{
+	struct elf_header_ident hdr;
+	size_t len;
+	size_t offset_shoff, offset_shnum, offset_shentsize;
+	uint64_t shoff_buffer;
+	uint16_t shnum, shentsize;
+	size_t loader_size, rootfs_offset;
+	enum mtdsplit_part_type type;
+	struct mtd_partition *parts;
+	int err;
+
+	len = sizeof(hdr);
+	err = mtdsplit_elf_read_mtd(mtd, 0, (uint8_t *)&hdr, len);
+	if (err)
+		return err;
+
+	if (be32_to_cpu(hdr.magic) != ELF_MAGIC) {
+		pr_debug("invalid ELF magic %08x\n", be32_to_cpu(hdr.magic));
+		return -EINVAL;
+	}
+
+	offset_shoff = sizeof(hdr);
+	offset_shnum = sizeof(hdr);
+	offset_shentsize = sizeof(hdr);
+
+	switch (hdr.class) {
+	case ELF_CLASS_32:
+		offset_shnum += offsetof(struct elf_header_32, shnum);
+		offset_shentsize += offsetof(struct elf_header_32, shentsize);
+		offset_shoff += offsetof(struct elf_header_32, shoff);
+		len = FIELD_SIZEOF(struct elf_header_32, shoff);
+		break;
+	case ELF_CLASS_64:
+		offset_shnum += offsetof(struct elf_header_64, shnum);
+		offset_shentsize += offsetof(struct elf_header_64, shentsize);
+		offset_shoff += offsetof(struct elf_header_64, shoff);
+		len = FIELD_SIZEOF(struct elf_header_64, shoff);
+		break;
+	default:
+		pr_debug("invalid ELF class %i\n", hdr.class);
+		return -EINVAL;
+	}
+
+	err = mtdsplit_elf_read_mtd(mtd, offset_shoff,
+				    (uint8_t *)&shoff_buffer, len);
+	if (err)
+		return err;
+
+	switch (hdr.class) {
+	case ELF_CLASS_32:
+		loader_size = *((uint32_t *)&shoff_buffer);
+		break;
+	case ELF_CLASS_64:
+		loader_size = *((uint64_t *)&shoff_buffer);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	len = sizeof(shnum);
+	err = mtdsplit_elf_read_mtd(mtd, offset_shnum,
+				    (uint8_t *)&shnum, len);
+	if (err)
+		return err;
+
+	len = sizeof(shentsize);
+	err = mtdsplit_elf_read_mtd(mtd, offset_shentsize,
+				    (uint8_t *)&shentsize, len);
+	if (err)
+		return err;
+
+	loader_size += shnum * shentsize;
+
+	err = mtd_find_rootfs_from(mtd, loader_size, mtd->size,
+				   &rootfs_offset, &type);
+	if (err)
+		return err;
+
+	if (rootfs_offset == mtd->size) {
+		pr_debug("no rootfs found in \"%s\"\n", mtd->name);
+		return -ENODEV;
+	}
+
+	parts = kzalloc(ELF_NR_PARTS * sizeof(*parts), GFP_KERNEL);
+	if (!parts)
+		return -ENOMEM;
+
+	parts[0].name = KERNEL_PART_NAME;
+	parts[0].offset = 0;
+	parts[0].size = rootfs_offset;
+
+	if (type == MTDSPLIT_PART_TYPE_UBI)
+		parts[1].name = UBI_PART_NAME;
+	else
+		parts[1].name = ROOTFS_PART_NAME;
+	parts[1].offset = rootfs_offset;
+	parts[1].size = mtd->size - rootfs_offset;
+
+	*pparts = parts;
+	return ELF_NR_PARTS;
+}
+
+static const struct of_device_id mtdsplit_elf_of_match_table[] = {
+	{ .compatible = "openwrt,elf" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, mtdsplit_elf_of_match_table);
+
+static struct mtd_part_parser mtdsplit_elf_parser = {
+	.owner = THIS_MODULE,
+	.name = "elf-loader-fw",
+	.of_match_table = mtdsplit_elf_of_match_table,
+	.parse_fn = mtdsplit_parse_elf,
+	.type = MTD_PARSER_TYPE_FIRMWARE,
+};
+
+static int __init mtdsplit_elf_init(void)
+{
+	register_mtd_parser(&mtdsplit_elf_parser);
+
+	return 0;
+}
+
+subsys_initcall(mtdsplit_elf_init);
